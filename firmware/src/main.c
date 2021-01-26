e5461d8d0e81e80e6126e180ae246f176da4ba61
varrocs <varrocs@gmail.com>
2021-01-26 20:29:01 +0100

    Add shell

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -18,11 +18,16 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/usb/usbd.h>
 #include <libopencm3/usb/cdc.h>
 #include <libopencm3/usb/hid.h>
+#include <libopencm3/cm3/systick.h>
+
+//#include "shell.h"
+#include "microrl/microrl.h"
 
 #define ENDP_ADDR_SRL_DATA_OUT  0x01
 #define ENDP_ADDR_SRL_DATA_IN   0x82
@@ -38,6 +43,70 @@
 
 #define UNUSED(x) ((void)x)
 
+#define KEY_MEDIA_VOLUMEUP 0xed
+#define KEY_MEDIA_VOLUMEDOWN 0xee
+
+#define KEY_VOLUMEUP 0x80 // Keyboard Volume Up
+#define KEY_VOLUMEDOWN 0x81 // Keyboard Volume Down
+
+usbd_device *global_usb_dev=NULL;
+static microrl_t shell;
+
+
+uint8_t key_to_write = 0;
+
+// ----------------------------------------------------------------------- MISC
+uint16_t usb_write_key(uint8_t key);
+
+static void led_up(void) {
+	gpio_clear(GPIOC, GPIO13);
+   // usb_write_key(KEY_VOLUMEUP);
+	//key_to_write = KEY_MEDIA_VOLUMEUP;
+	key_to_write = KEY_VOLUMEUP;
+}
+
+static void led_down(void) {
+	gpio_set(GPIOC, GPIO13);
+	//usb_write_key(KEY_VOLUMEDOWN);
+	// key_to_write = KEY_MEDIA_VOLUMEDOWN;
+	key_to_write = KEY_VOLUMEDOWN;
+}
+
+uint16_t usb_write_key(uint8_t key)
+{
+	uint8_t addr = ENDP_ADDR_HID_IN;
+	// uint8_t addr = ENDP_ADDR_MKEYS_IN;
+	uint8_t buf[8]={
+		0, // modifiers
+		0, // reserverd
+		0, // leds,
+		key,
+		0,//ps2keyboard.usb_keys[1],
+		0,//ps2keyboard.usb_keys[2],
+		0,//ps2keyboard.usb_keys[3],
+		0,//ps2keyboard.usb_keys[4],
+	};
+
+	return usbd_ep_write_packet(global_usb_dev, addr, buf, sizeof buf);
+}
+
+// ----------------------------------------------------------------------- SHELL
+
+static void shell_print_callback (const char * str) {
+	const size_t len = strlen(str);
+	usbd_ep_write_packet(global_usb_dev, ENDP_ADDR_SRL_DATA_IN, str, len);
+}
+
+static int shell_execute_callback(int argc, const char* const* argv) {
+	if (argc == 0) return 0;
+
+	if (strcmp(argv[0], "up") == 0) {
+		led_up();
+	} else if (strcmp(argv[0], "down") == 0) {
+		led_down();
+	}
+	return 0;
+}
 
 static const struct usb_device_descriptor dev = {
 	.bLength = USB_DT_DEVICE_SIZE,
@@ -397,7 +466,7 @@ static enum usbd_request_return_codes hid_control_request_interface(usbd_device 
 {
     UNUSED(complete);
     UNUSED(usbd_dev);
-    //UNUSED(buf);
+    UNUSED(buf);
 
     if(req->bmRequestType==(USB_REQ_TYPE_CLASS|USB_REQ_TYPE_INTERFACE) /*0x21*/)
     {
@@ -422,29 +491,29 @@ static enum usbd_request_return_codes hid_control_request_interface(usbd_device 
     }
 
     // TODO COPIED hid_control_request
-    if((req->bmRequestType == (USB_REQ_TYPE_IN|USB_REQ_TYPE_STANDARD|USB_REQ_TYPE_INTERFACE) /*0x81*/) &&
-       (req->bRequest == USB_REQ_GET_DESCRIPTOR /*0x06*/) &&
-       (req->wValue == 0x2200 /*Descriptor Type (high byte) and Index (low byte)*/))
-    {
-        // req->wIndex is the interface number
-        if(req->wIndex==IFACE_NUMBER_HID)
-        {
-            //printf("Got request for keyboard report descriptor\n");
-            *buf = (uint8_t *)hid_keyboard_report_descriptor;
-            *len = sizeof(hid_keyboard_report_descriptor);
-        }
-        else if(req->wIndex==IFACE_NUMBER_MKEYS)
-        {
-            //printf("Got request for mediakey report descriptor\n");
-            *buf = (uint8_t*)hid_mediakey_report_descriptor;
-            *len = sizeof(hid_mediakey_report_descriptor);
-        }
-        else
-        {
-            return USBD_REQ_NOTSUPP;
-        }
-        return USBD_REQ_HANDLED;
-    }
+    // if((req->bmRequestType == (USB_REQ_TYPE_IN|USB_REQ_TYPE_STANDARD|USB_REQ_TYPE_INTERFACE) /*0x81*/) &&
+    //    (req->bRequest == USB_REQ_GET_DESCRIPTOR /*0x06*/) &&
+    //    (req->wValue == 0x2200 /*Descriptor Type (high byte) and Index (low byte)*/))
+    // {
+    //     // req->wIndex is the interface number
+    //     if(req->wIndex==IFACE_NUMBER_HID)
+    //     {
+    //         //printf("Got request for keyboard report descriptor\n");
+    //         *buf = (uint8_t *)hid_keyboard_report_descriptor;
+    //         *len = sizeof(hid_keyboard_report_descriptor);
+    //     }
+    //     else if(req->wIndex==IFACE_NUMBER_MKEYS)
+    //     {
+    //         //printf("Got request for mediakey report descriptor\n");
+    //         *buf = (uint8_t*)hid_mediakey_report_descriptor;
+    //         *len = sizeof(hid_mediakey_report_descriptor);
+    //     }
+    //     else
+    //     {
+    //         return USBD_REQ_NOTSUPP;
+    //     }
+    //     return USBD_REQ_HANDLED;
+    // }
 
    // printf("Got USB unsupported request: bmRequestType=%#x\nbRequest=%#x\nwValue=%#x\nwIndex=%#x\nwLenght=%#x\n\n",
    //        req->bmRequestType,
@@ -507,8 +576,20 @@ static void cdcacm_data_rx_cb(usbd_device *usbd_dev, uint8_t ep)
 	char buf[64];
 	int len = usbd_ep_read_packet(usbd_dev, ENDP_ADDR_SRL_DATA_OUT, buf, 64);
 
+
 	if (len) {
-		usbd_ep_write_packet(usbd_dev, ENDP_ADDR_SRL_DATA_IN, buf, len);
+
+		//usbd_ep_write_packet(usbd_dev, ENDP_ADDR_SRL_DATA_IN, buf, len);
+
+
+		//char* response;
+		//size_t response_length;
+		//on_incoming_data(buf, len, &response, &response_length);
+		//usbd_ep_write_packet(usbd_dev, ENDP_ADDR_SRL_DATA_IN, response, response_length);
+		for (int i = 0; i < len; i++) {
+			microrl_insert_char (&shell, buf[i]);
+		}
+
 		buf[len] = 0;
 	}
 }
@@ -548,6 +629,16 @@ static void cdcacm_set_config(usbd_device *usbd_dev, uint16_t wValue)
 			hid_control_request_interface);
 }
 
+
+void sys_tick_handler(void) {
+	if (key_to_write != 0) {
+		int16_t written = usb_write_key(key_to_write);
+		if (written) {
+			key_to_write=0;
+		}
+	}
+}
+
 int main(void)
 {
 	int i;
@@ -556,18 +647,31 @@ int main(void)
 
 	rcc_clock_setup_in_hse_8mhz_out_72mhz();
 
-	rcc_periph_clock_enable(RCC_GPIOC);
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
+	/* SysTick interrupt every N clock pulses: set reload to N-1 */
+	systick_set_reload(99999);
+	systick_interrupt_enable();
+	systick_counter_enable();
 
-	gpio_set(GPIOC, GPIO11);
+	rcc_periph_clock_enable(RCC_GPIOC);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_set(GPIOA, GPIO12);
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO12);
+
 	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO11);
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 
 	usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
 	usbd_register_set_config_callback(usbd_dev, cdcacm_set_config);
 
 	for (i = 0; i < 0x800000; i++)
 		__asm__("nop");
-	gpio_clear(GPIOC, GPIO11);
+	gpio_clear(GPIOA, GPIO12);
+
+	global_usb_dev = usbd_dev;
+	microrl_init(&shell, shell_print_callback);
+	microrl_set_execute_callback (&shell, shell_execute_callback);
 
 	while (1)
 		usbd_poll(usbd_dev);
